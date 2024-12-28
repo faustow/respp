502f5a876cc40b43d8f8e143d90ded012af13bc1
Fausto Woelflin <faustow@gmail.com>
2024-12-27 23:41:21 -0300

    Add search tests

--- properties/tests/test_views.py
+++ properties/tests/test_views.py
@@ -60,3 +60,45 @@ class PropertiesAPITestCase(TestCase):
         response = self.client.post(self.url, invalid_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Property.objects.count(), amount)  # No new property should be created
+
+
+class PropertiesAPISearchTestCase(TestCase):
+    def setUp(self):
+        Property.objects.all().delete()
+        self.url = "/api/properties/"
+        # Crear propiedades de prueba
+        Property.objects.create(
+            lotarea=10000, overallqual=7, overallcond=5, centralair=1,
+            fullbath=2, bedroomabvgr=3, garagecars=2,
+            yrsold=2022, mosold=6, saleprice=250000
+        )
+        Property.objects.create(
+            lotarea=9500, overallqual=6, overallcond=5, centralair=1,
+            fullbath=2, bedroomabvgr=3, garagecars=2,
+            yrsold=2023, mosold=5, saleprice=240000
+        )
+        Property.objects.create(
+            lotarea=10000, overallqual=7, overallcond=5, centralair=1,
+            fullbath=2, bedroomabvgr=3, garagecars=2,
+            yrsold=2023, mosold=7, saleprice=260000
+        )
+
+    def test_search_closest_property(self):
+        query_params = {
+            "lotarea": 10000,
+            "overallqual": 7,
+            "overallcond": 5,
+            "centralair": "Yes",
+            "fullbath": 2,
+            "bedroomabvgr": 3,
+            "garagecars": 2,
+        }
+        response = self.client.get(self.url, query_params)
+
+        # Verificar que la respuesta sea 200 OK
+        self.assertEqual(response.status_code, 200)
+
+        # Verificar que la propiedad más reciente sea devuelta
+        data = response.json()
+        self.assertEqual(data["yrsold"], 2023)
+        self.assertEqual(data["mosold"], 7)
